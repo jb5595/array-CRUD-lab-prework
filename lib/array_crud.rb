c64c472a44cd0d434cdfda7d832bf31b42499c4d
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 02:15:00 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  return array.new
+  return Array.new
   
 end
 
 def add_element_to_end_of_array(array, element)
+  array.push(element)
   
 end
 
